449b445b09ecace3d9e78496a77581e9b225207b
AriosJentu <darthpoezd@gmail.com>
2017-10-12 09:06:35 +1000

    Added Marks

--- creater.py
+++ creater.py
@@ -31,24 +31,26 @@ def generate_random_name():
 
 database = db.connect("base.db")
 cursor = database.cursor()
-cursor.execute("CREATE TABLE Students (ID Integer PRIMARY KEY NOT NULL, Name Text, Born Date, Group_ID Integer);")
+cursor.execute("CREATE TABLE Students (ID Integer PRIMARY KEY NOT NULL, Name Text, Born DATETIME, Group_ID Integer);")
 cursor.execute("CREATE TABLE Groups (ID Integer, Name Text);")
-cursor.execute("CREATE TABLE Subjects (ID Integer, Name Text);")
+cursor.execute("CREATE TABLE Subjects (ID Integer, Name Text, Group_ID Integer);")
 cursor.execute("CREATE TABLE Marks (Student_ID Integer, Subject_ID Integer, Mark Integer);")
 #cursor.execute("CREATE TABLE Gen (GenID SERIAL PRIMARY KEY);")
+
+studcount = 100
 
 for i in range(len(groups)):
 	cursor.execute("INSERT INTO Groups VALUES (%i, '%s');"%(i+1, groups[i]))
 
 for i in range(len(subjs)):
-	cursor.execute("INSERT INTO Subjects VALUES (%i, '%s');"%(i+1, subjs[i]))
+	cursor.execute("INSERT INTO Subjects VALUES (%i, '%s', %i);"%(i+1, subjs[i], randrange(len(subjs))+1))
 
-for i in range(1, 101):
+for i in range(1, studcount+1):
 	fullname = generate_random_name()
 	cursor.execute("INSERT INTO Students VALUES (%i, '%s', '%.2i-%.2i-%i', %i);"%( i, fullname, randrange(1, 30), randrange(1, 12+1), randrange(1995, 2000), randrange(len(groups))+1) )
 
-for i in range(1, 101, 3):
-	st_id = i+randrange(1, 3)
+for i in range(1, 100001):
+	st_id = randrange(i%100 + 1, studcount+1)
 	sub_id = randrange(len(subjs))+1
 	mark = randrange(2, 6)
 	cursor.execute("INSERT INTO Marks VALUES (%i, %i, %i);"%(st_id, sub_id, mark))
